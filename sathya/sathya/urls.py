4260859d6d52b0b31bdfa13e996e6725c0eee38f
SathyaPriyan06 <priyansathya2008@gmail.com>
2025-12-20 20:54:01 +0530

    Success

---
+++ sathya/sathya/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('lampspower/',views.lampspower,name="lampspower"),
+    path('',views.lampspower,name="lampspower")
+]
+
